6337e6087b29bdd4a598bcddc9d2e7ceace86b15
accordingtojim <carradorejimmy@gmail.com>
2025-01-27 22:00:46 +1030

    -working on test_1 that approach is not viable, it need to open just 1 session, login and then look for links navigate straightforward and download

--- main_test.py
+++ main_test.py
@@ -4,7 +4,7 @@ from import_sqlite import import_json_to_sqlite
 import argparse  # Aggiunto argparse
 import os
 from bs4 import BeautifulSoup
-from undetc_test import fetch_html_from_links
+from test_playwright import fetch_html_from_links
 #from sqlite_import import import_json_to_sqlite
 #from analyzing_pdf import custom_data_extraction,consolidate_json
 
@@ -15,6 +15,9 @@ save_directory = "downloads"
 name_dump = 'debug.json'
 name_link = 'links.json'
 
+download_html_folder = "download"
+os.makedirs(download_html_folder, exist_ok=True)
+
 lista_comune = ['busto-arsizio', 'gallarate', 'castellanza','lonate-pozzolo', 'samarate', 'cardano-al-campo', 'varese','pavia']
 lista_provincia = ["varese","varese","varese","varese","varese","varese","varese","pavia"]
 lista_province = ["sondrio","trento","bolzano-bozen"]
@@ -22,7 +25,7 @@ lista_province = ["sondrio","trento","bolzano-bozen"]
 
 len_comune = len(lista_comune)
 len_provincia = len(lista_provincia)
-if 0 :
+if 1 :
     for element in range(0,len_comune):
         nuovi_link = extract_auction_links_from_page_comune(lista_comune[element], lista_provincia[element],'affitto','all')
         links.update(nuovi_link)
@@ -40,8 +43,8 @@ ids = extract_ids_from_links(links)
 
 
 
-if 1:
-    fetch_html_from_links(links, save_directory)
+if 0:
+    fetch_html_from_links(links,download_html_folder)
    
 for id in ids:
     houses.append(extract_house_details(f"{save_directory}/{id}.json"))
@@ -49,7 +52,7 @@ with open(f"{name_dump}", 'w', encoding='utf-8') as file:
         json.dump(houses, file, indent=4, ensure_ascii=False)
 
 #Debug
-if 1:
+if 0:
     import_json_to_sqlite(name_dump,'house.db')
 
     
